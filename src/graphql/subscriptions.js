16298cf3f086154a7c0490020d320bcf6b14f50e
tatoclemente <tatoapuros@gmail.com>
2023-09-27 01:01:47 -0300

    Inicializacion del repositorio

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTask = /* GraphQL */ `
+  subscription OnCreateTask($filter: ModelSubscriptionTaskFilterInput) {
+    onCreateTask(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateTask = /* GraphQL */ `
+  subscription OnUpdateTask($filter: ModelSubscriptionTaskFilterInput) {
+    onUpdateTask(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteTask = /* GraphQL */ `
+  subscription OnDeleteTask($filter: ModelSubscriptionTaskFilterInput) {
+    onDeleteTask(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
